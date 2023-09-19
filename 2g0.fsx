24649b44e009fc14d730318cc9dc7d11576a5cf6
Johnsonhannes <58007253+Johnsonhannes@users.noreply.github.com>
2023-09-19 13:12:52 +0200

    Update 2g0.fsx

--- 2g0.fsx
+++ 2g0.fsx
@@ -1,1 +1,14 @@
 
+
+type vec = float * float
+
+let add ((x1, y1) : vec) ((x2, y2) : vec) : vec = 
+    (x1 + x2, y1+ y2)
+
+let mul ((x, y) : vec) (c : float) : vec = 
+    ( c * x, c * y)
+
+let rot ((x, y) : vec) (a : float) : vec = 
+    ((x * cos (a) - y * sin(a)), (x * sin(a) + y * cos(a)))
+
+printfn "%A" (rot (1.0, 1.0) System.Math.PI)
