2f2b7ff64c6390304b6d2f88fb946ef3916502af
Johnsonhannes <58007253+Johnsonhannes@users.noreply.github.com>
2023-09-19 13:26:22 +0200

    Update 2g0.fsx

--- 2g0.fsx
+++ 2g0.fsx
@@ -4,7 +4,9 @@ open System
 
 type vec = float * float
 
-///<summary> add tager 2 vektorer ind og adderer dem (legge begge koordinater sammen)
+///<summary> add tager 2 vektorer ind og adderer dem (lægger begge koordinater sammen)
+///<param> tager 2 vec
+///<returns> vec som er summen af de 2 vektorer
 
 let add ((x1, y1) : vec) ((x2, y2) : vec) : vec = 
     (x1 + x2, y1+ y2)
