c3789ed535970ea53730503f1c3c0d068daf5e3c
Johnsonhannes <58007253+Johnsonhannes@users.noreply.github.com>
2023-09-21 13:18:48 +0200

    Update 2g0.fsx

--- 2g0.fsx
+++ 2g0.fsx
@@ -11,8 +11,18 @@ type vec = float * float
 let add ((x1, y1) : vec) ((x2, y2) : vec) : vec = 
     (x1 + x2, y1+ y2)
 
+///<summary>  mul skalerer en vektor med skalaren c </summary>
+///<param> (x,y) : vec </param>
+///<param> c: float </param>
+///<returns> (x,y)-vektoren skaleret med c </returns>
+
 let mul ((x, y) : vec) (c : float) : vec = 
     ( c * x, c * y)
+
+///<summary> rot tager en vektor og roterer den med a radianer mod uret </summary>
+///<param> (x,y) : vec </param>
+///<param> a : float </param>
+///<returns> (x,y)-vektoren drejet a radianer </returns>
 
 let rot ((x, y) : vec) (a : float) : vec = 
     ((x * cos (a) - y * sin(a)), (x * sin(a) + y * cos(a)))
